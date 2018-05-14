5df525f63c9f7dbcf60a0b033daa4398b6c2c965
Eitan Ben David <eitanbendavid96@gmail.com>
2018-05-14 21:39:22 +0300

    start is now implimented

--- app.py
+++ app.py
@@ -75,14 +75,17 @@ def get_up_time():
 def command_switch_response(command):
     print("switching the command:{}".format(command))
     
-    commands = {'help': "commands available:{}".format(
+    helpText = "commands available:{}".format(
             "\nHelp - returns the available commands." +
             "\nTime - returns the current time." +
             "\nCounter - Counter of this command." +
-            "\nUptime - how long am i up."),
-        'time': datetime.datetime.now,
-        'counter': using_Counter_Up,
-        'uptime': get_up_time}
+            "\nUptime - how long am i up.")
+
+    commands = {'/start': helpText,
+                'help': helpText,
+                'time': datetime.datetime.now,
+                'counter': using_Counter_Up,
+                'uptime': get_up_time}
 
     response = commands.get(command, "no such command")
     if callable(response):
